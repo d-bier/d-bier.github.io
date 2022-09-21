95cd5397224a10174cbaeb46bdb5874d3b5fb06b
d-bier <96543140+d-bier@users.noreply.github.com>
2022-09-21 08:39:44 +0200

    Import

---
+++ data.js
@@ -0,0 +1,440 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-atrium",
+      "name": "Atrium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1019594550257672,
+          "pitch": 0.025641081043442426,
+          "rotation": 0.7853981633974483,
+          "target": "1-atrium---2"
+        },
+        {
+          "yaw": 2.613610851790833,
+          "pitch": 0.12810560048310826,
+          "rotation": 6.283185307179586,
+          "target": "2-atrium---3"
+        },
+        {
+          "yaw": 2.061714120405192,
+          "pitch": -0.05407989007980518,
+          "rotation": 5.497787143782138,
+          "target": "3-foyer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.6743386368071036,
+          "pitch": 0.045267693557603295,
+          "title": "Haupteingang - Erdgeschoss",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-atrium---2",
+      "name": "Atrium - 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0254661042401505,
+          "pitch": 0.27303441541857154,
+          "rotation": 5.497787143782138,
+          "target": "0-atrium"
+        },
+        {
+          "yaw": -0.3246616744562072,
+          "pitch": 0.0975486235975982,
+          "rotation": 0.7853981633974483,
+          "target": "2-atrium---3"
+        },
+        {
+          "yaw": -0.6502139463389724,
+          "pitch": 0.09910211102380728,
+          "rotation": 5.497787143782138,
+          "target": "7-vorlesungssaal-26_27"
+        },
+        {
+          "yaw": -3.0967084352648087,
+          "pitch": 0.05353376672557175,
+          "rotation": 0.7853981633974483,
+          "target": "7-vorlesungssaal-26_27"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-atrium---3",
+      "name": "Atrium - 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.38019000691816984,
+          "pitch": 0.30248352327248895,
+          "rotation": 0.7853981633974483,
+          "target": "0-atrium"
+        },
+        {
+          "yaw": 1.9162126065001734,
+          "pitch": 0.0790031782032834,
+          "rotation": 5.497787143782138,
+          "target": "1-atrium---2"
+        },
+        {
+          "yaw": 1.516670201751145,
+          "pitch": -0.14635600243127023,
+          "rotation": 0,
+          "target": "3-foyer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0429826054321323,
+          "pitch": 0.10304261453705443,
+          "title": "Toiletten",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.0058792724858208,
+          "pitch": 0.13992011266814863,
+          "title": "Zentrum Digitalisierung",
+          "text": "<a href=\"https://www.hhz.de/de/standort/zentrum-digitalisierung/\">Zentrum Digitalisierung : Herman Hollerith Zentrum (hhz.de)</a>"
+        }
+      ]
+    },
+    {
+      "id": "3-foyer",
+      "name": "Foyer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0905404307533324,
+          "pitch": 0.16913861597221036,
+          "rotation": 3.141592653589793,
+          "target": "2-atrium---3"
+        },
+        {
+          "yaw": -1.0658850868500132,
+          "pitch": 0.10622312451179106,
+          "rotation": 0,
+          "target": "6-kaffekche"
+        },
+        {
+          "yaw": -1.9808430134395003,
+          "pitch": 0.06996990706967665,
+          "rotation": 5.497787143782138,
+          "target": "5-have-a-break-raum"
+        },
+        {
+          "yaw": -0.16692957334693048,
+          "pitch": 0.061021276495750953,
+          "rotation": 0.7853981633974483,
+          "target": "4-gang-1-stockwerk"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.8096967216024673,
+          "pitch": 0.14719458588549017,
+          "title": "Eingang - 1. Stockwerk",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.6627309729503104,
+          "pitch": 0.07238268576415408,
+          "title": "Büros der Professoren",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.5203759577719165,
+          "pitch": 0.08490330428535486,
+          "title": "Besprechungsraum",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-gang-1-stockwerk",
+      "name": "Gang 1. Stockwerk",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6394328017997033,
+          "pitch": 0.22886486575589338,
+          "rotation": 5.497787143782138,
+          "target": "3-foyer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7425718699273336,
+          "pitch": 0.1037452628807003,
+          "title": "Toiletten",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.5791750635310446,
+          "pitch": 0.03300992410744641,
+          "title": "Besprechungsraum",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-have-a-break-raum",
+      "name": "Have-a-Break-Raum",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5594911132164535,
+          "pitch": 0.007714385759491904,
+          "rotation": 0.7853981633974483,
+          "target": "3-foyer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-kaffekche",
+      "name": "Kaffeküche",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7715437891368939,
+          "pitch": 0.19602886619319904,
+          "rotation": 0.7853981633974483,
+          "target": "3-foyer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2641691818069773,
+          "pitch": 0.13273886382984657,
+          "title": "Büro Antje Resmini (Teamassistentin)",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "7-vorlesungssaal-26_27",
+      "name": "Vorlesungssaal 26_27",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20585608990698745,
+          "pitch": 0.0663480941915573,
+          "rotation": 0.7853981633974483,
+          "target": "2-atrium---3"
+        },
+        {
+          "yaw": 1.641500490072719,
+          "pitch": 0.03201620398791505,
+          "rotation": 5.497787143782138,
+          "target": "1-atrium---2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Rundgang HHZ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
